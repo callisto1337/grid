ba37b035f07794bc8e036e8fed94fd204fad248f
m.lamotkin <hazardous333@gmail.com>
2023-11-03 13:05:53 +0300

    added demo page

---
+++ src/demo/Demo.vue
@@ -0,0 +1,17 @@
+<template>
+  <Grid :spacing="['8px', '16px']">
+    <GridItem :size="6">First item</GridItem>
+    <GridItem :size="6">Second item</GridItem>
+    <GridItem :size="12">Third item</GridItem>
+  </Grid>
+</template>
+
+<script lang="ts" setup>
+import { Grid, GridItem } from '../';
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+</style>
---
+++ src/demo/index.ts
@@ -0,0 +1,6 @@
+import { createApp } from 'vue';
+import Demo from './Demo.vue';
+
+const app = createApp(Demo);
+
+app.mount('#app');
